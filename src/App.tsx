76b9f55bae95cf34358ea9a469ef37e2038a7477
DomingosManuelFranco <manuelfranco070@gmail.com>
2022-06-23 11:50:43 +0100

    some bugs on Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import Header from "./components/Header";
-import Lessons from "./components/Lessons";
-import Sidebar from "./components/Sidebar";
-import Video from "./components/Video";
+import Event from "./pages/Event";
+import "./styles/global.css";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Sidebar />
-      <Video />
-      <Lessons />
+    <div>
+      <Event />
     </div>
   );
 }
