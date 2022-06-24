dd29382d3f35b458f69038451a3793284c3e8629
DomingosManuelFranco <manuelfranco070@gmail.com>
2022-06-24 02:29:02 +0100

    fixed a misspelled css propriety

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -6,7 +6,7 @@ const Event = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex fl">
+      <main className="flex flex-1">
         <Video />
         <Sidebar />
       </main>
