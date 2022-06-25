8206bc1abea1c3ee259a13d3feee147ca06e543d
DomingosManuelFranco <manuelfranco070@gmail.com>
2022-06-25 23:06:06 +0100

    static pages created

--- src/components/Lessons.tsx
+++ src/components/Lessons.tsx
@@ -1,5 +1,19 @@
+import { CheckCircle } from "phosphor-react";
+
 const Lessons = () => {
-  return <div>Lessons</div>;
+  return (
+    <a>
+      <span className="text-gray-300">Tuesday, March 13th - 9:00 AM</span>
+
+      <div className="rounded border border-gray-500 p-4 mt-2">
+        <header className="flex items-center justify-between">
+          <span className="text-sm text-blue-500 font-medium">
+            <CheckCircle size={20} />
+          </span>
+        </header>
+      </div>
+    </a>
+  );
 };
 
 export default Lessons;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,20 @@
+import Lessons from "./Lessons";
+
 const Sidebar = () => {
   return (
-    <aside className="w-[348] bg-gray-700 border-l border-gray-600"></aside>
+    <aside className="w-[348] bg-gray-700 p-6 border-l border-gray-600">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Lessons Schedule
+      </span>
+      <div className="flex flex-col gap-8">
+        <Lessons />
+        <Lessons />
+        <Lessons />
+        <Lessons />
+        <Lessons />
+        <Lessons />
+      </div>
+    </aside>
   );
 };
 
